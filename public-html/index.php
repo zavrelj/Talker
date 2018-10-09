3a38edbbec55b69f347ed9fb84f1d971ad6b1b0c
Jan Zavrel <jan@zavrel.net>
2018-10-09 13:43:10 +0200

    Bootstrap starter template
    Signup form
    
    sign-up form

--- public-html/index.php
+++ public-html/index.php
@@ -1,22 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>TALKER</title>
-
-    <!-- Bootstrap CSS-->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
   <body>
-    <h1>TALKER</h1>
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-12 col-md-auto"><h1>TALKER | SIGN UP</h1></div>
+        </div>
+
+        <hr><br>
+
+        <div class="row">
+            <div class="col-6">
+                <form>
+                    <div class="form-group">
+                        <label for="formSignUpEmail">Email address</label>
+                        <input type="email" class="form-control" id="formSignUpEmail" placeholder="Enter your email address">
+                    </div>
+                    <div class="form-group">
+                        <label for="formSignUpPassword">Password</label>
+                        <input type="password" class="form-control" id="formSignUpPassword" placeholder="Enter your password">
+
+                        <input type="password" class="form-control mt-4" id="formSignUpPasswordConf" placeholder="Confirm your password">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+
+            <div class="col-6">
+                <p>Hello and welcome to Talker! We are very happy that you want to join our great community!</p>
+                <p>Please, enter your email and password. Your must have access to your email because we will send
+            a confirmation code to that address. Your password must be between 8 and 16 characters long, with at
+            least one uppercase and one lowercase character, one number and one special character (@, *, $ or #).</p>
+                <p>We hope you'll enjoy Talker!</p>
+            </div>
+        </div>
+    </div>
 
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <!-- Optional Javascript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   </body>
 </html>
