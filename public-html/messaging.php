d4c71ea0a21032a58adfea00c2eeed2a13334fed
Jan Zavrel <jan@zavrel.net>
2018-11-22 12:38:25 +0100

    Reading messages

--- public-html/messaging.php
+++ public-html/messaging.php
@@ -58,14 +58,57 @@ if ($dbUserRow["user_verified"] == 0) {
                     <?php } ?>
                 </div>
 
-                <button type="submit" id="formMessagingSubmit" name="formMessagingSubmit" class="btn btn-primary btn-success">Send</button>
+                <button type="submit" id="formMessagingSubmit" name="formMessagingSubmit" class="btn btn-primary btn-success mb-5">Send</button>
             </form>
         </div>
     </div>
 
+    <?php
+	$db_data = array($_SESSION["uid"], $_SESSION["uid"]);
+	$dbMessagesList = phpFetchAllDB('SELECT * FROM messages WHERE message_sender_id = ? OR message_recipient_id = ? ORDER BY message_date DESC', $db_data);
+	$db_data = "";
+    ?>
+
+    <p><strong>Latest messages</strong></p>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <table class="table">
+
+            <?php foreach ($dbMessagesList as $dbMessageRow) { ?>
+            <tr>
+                <td class="message_header">
+                    <?php if ($dbMessageRow["message_sender_id"] == $_SESSION["uid"]) { ?>
+                        TO: <?php echo phpGetUserEmail($dbMessageRow["message_recipient_id"]); ?>
+                    <?php }else{ ?>
+                        FROM: <?php echo phpGetUserEmail($dbMessageRow["message_sender_id"]); ?>
+                    <?php } ?>
+                        | DATE: <?php echo $dbMessageRow["message_date"]; ?>
+
+                    <?php if ($dbMessageRow["message_sender_id"] == $_SESSION["uid"] && $dbMessageRow["message_read_by_recipient"] == 1) { ?>
+                        | READ BY RECIPIENT
+                    <?php } ?>
+
+                </td>
+            </tr>
+
+            <tr><td class="message_content"><?php echo $dbMessageRow["message_content"]; ?></td></tr>
+            <?php } ?>
+
+            </table>
+        </div>
+    </div>
+
+
     <script src="messaging.js"></script>
 
 <?php
+
+//UPDATE MESSAGES READ BY RECIPIENT
+$db_data = array($_SESSION["uid"]);
+phpModifyDB('UPDATE messages SET message_read_by_recipient = 1 WHERE message_recipient_id = ?', $db_data);
+$db_date = "";
+
 
 $_SESSION["messaging_recipient"] = "";
 
--- public-html/system.ctrl.php
+++ public-html/system.ctrl.php
@@ -176,6 +176,13 @@ function phpFetchAllDB($db_query, $db_data) {
     return $statement->fetchAll(PDO::FETCH_ASSOC);
 }
 
+// Return user's email based on his id
+function phpGetUserEmail($user_id) {
+	$db_data = array($user_id);
+	$db_result = phpFetchDB('SELECT user_email FROM users WHERE user_id = ?', $db_data);
+	return $db_result['user_email'];
+}
+
 function phpSendEmail($to, $subject, $content) {
 
     //Create a new PHPMailer instance
